0cb13f3a60ac65c8aa254eff924981af93f244f2
Sebastian Javier Maidana <seba_maidana_3@hotmail.com>
2025-09-02 15:08:45 -0300

    server php time zone argentina

--- src/index.php
+++ src/index.php
@@ -8,7 +8,12 @@
 <body>
     <p>Entorno PHP</p>
     <?php
+        echo date('Y-m-d H:i:s');
+        echo "<br>";
+        echo date_default_timezone_get();
+        echo "<br>";
         var_dump(gd_info());
+        echo "<br>";
         phpinfo();
     ?>
 </body>
